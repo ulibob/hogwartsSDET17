3ee0ce39255d9fa04f0ce22f6a853cbb538f39e3
ulibo_1 <ulibo_1@outlook.com>
2021-01-30 17:51:35 +0800

    完成：    pytest测试用例，参数化

--- lesson1/testing/test_calculator.py
+++ lesson1/testing/test_calculator.py
@@ -1,6 +1,4 @@
 # -*- coding:utf-8 -*-
-import os
-
 import pytest
 import yaml
 from lesson1.pythoncode.Caculator import MyCalculator
@@ -10,7 +8,6 @@ from lesson1.pythoncode.Caculator import MyCalculator
 def get_datas():
     with open("./datas/calc.yml", encoding="utf-8") as f:
         datas = yaml.safe_load(f)
-
     return datas
 
 
@@ -24,13 +21,14 @@ class TestCalc:
     divid_data: dict = datas["divid"]["datas"], datas["divid"]["ids"]
 
     # 前置处理
+    # set_class只在类方法前，前置处理一次，与此类中的测试方法个数无关
     def setup_class(self):
         # 计算器的实例化
         self.calc = MyCalculator()
-
         print("开始计算:\n")
 
     # 后置处理
+    # teardown_class只在类方法完后，后置处理一次，与此类中的测试方法个数无关
     def teardown_class(self):
         print("计算结束.")
 
